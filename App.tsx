ab49ad269646840a74edec90b161140cafc9893d
umut imamoglu <umutimamoglu19@gmail.com>
2024-07-19 21:06:03 +0300

    create ract-native live  chat application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import tw from 'twrnc';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`flex-1 justify-center items-center bg-blue-500`}>
+      <Text style={tw`text-white text-lg`}>Hello, Tailwind!</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
